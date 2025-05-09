c6194234d3b236a59e56d765654e49f2276bc6dd
AngyBeth <angybeth18@gmail.com>
2025-05-09 10:30:23 -0500

    Actualización de datos o funcionalidades

--- src/main/java/com/moviles/productos/ProductoController.java
+++ src/main/java/com/moviles/productos/ProductoController.java
@@ -14,10 +14,10 @@ public class ProductoController {
     public List<Producto> obtenerRecomendados() {
         List<Producto> lista = new ArrayList<>();
         lista.add(new Producto("Pastel Flowers", "Libre de gluten", "Pasteles", 250.0, "https://dulcefina.com.pe/wp-content/uploads/2021/06/WhatsApp-Image-2021-06-26-at-10.42.37-AM-2-416x420.jpg"));
-        lista.add(new Producto("Pastel Corazón", "Libre de colorantes", "Pasteles", 360.5, "https://dulcefina.com.pe/wp-content/uploads/2021/09/calado-costado-416x416.png"));
-        lista.add(new Producto("Cupcakes Mamá", "Libre de preservantes", "Cupcakes", 80.0, "https://dulcefina.com.pe/wp-content/uploads/2025/04/ESTUCHE-CUPCAKES-X1-416x416.jpg"));
-        lista.add(new Producto("Galletas Dog", "Alto en proteínas", "Galletas", 40.0, "https://dulcefina.com.pe/wp-content/uploads/2023/05/brissa-4-416x416.jpg"));
-        lista.add(new Producto("Pastel Fly", "Libre de gluten", "Pasteles", 180.0, "https://dulcefina.com.pe/wp-content/uploads/2023/05/brissa-4-416x416.jpg"));
+        lista.add(new Producto("Pastel Corazón", "Libre de gluten", "Pasteles", 360.5, "https://dulcefina.com.pe/wp-content/uploads/2021/09/calado-costado-416x416.png"));
+        lista.add(new Producto("Cupcakes Mamá", "Libre de gluten", "Cupcakes", 80.0, "https://dulcefina.com.pe/wp-content/uploads/2025/04/ESTUCHE-CUPCAKES-X1-416x416.jpg"));
+        lista.add(new Producto("Galletas Dog", "Libre de gluten", "Cookies", 40.0, "https://dulcefina.com.pe/wp-content/uploads/2023/05/brissa-4-416x416.jpg"));
+        lista.add(new Producto("Pastel Fly", "Libre de gluten", "Pasteles", 180.0, "https://dulcefina.com.pe/wp-content/uploads/2025/02/mariposa_0001_Capa-2-416x416.jpg"));
         
         return lista;
     }
